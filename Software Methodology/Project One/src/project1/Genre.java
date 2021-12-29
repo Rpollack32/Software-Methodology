7e94514fa801f3c8eca59f04b379bec00ed83df3
Rpollack32 <47619503+Rpollack32@users.noreply.github.com>
2021-12-29 01:36:41 -0500

    Add files via upload

---
+++ Software Methodology/Project One/src/project1/Genre.java
@@ -0,0 +1,15 @@
+package projectOne;
+
+/**
+ * An enum class that deals with the different genres
+ * @author Ryan Pollack, Michael Kang
+ */
+public enum Genre
+{
+
+    Classical,
+    Country,
+    Jazz,
+    Pop,
+    Unknown
+}
